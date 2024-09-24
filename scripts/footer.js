04f262028becc1b55e687510e5031b1900b725c4
MANTOOO <subhajeetchowdhury954@gmail.com>
2024-09-24 21:21:03 +0530

    update

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+const footerData = `
+  <div class="brand_name">
+    <p class="band_text">&#169 2024 - Mantomart</p>
+  </div>
+`;
+
+const footer = document.querySelector('.footer');
+footer.innerHTML = footerData;
---
+++ scripts/header.js
@@ -0,0 +1,25 @@
+const headerData = `
+  <div class="logo_div">
+    <img src="images/logos_and_icons/logo.png" alt="Mantomart" class="logo"/>
+  </div>
+  <div class="search_bar">
+    <div class="input_search_cont">
+      <input type="text" placeholder="Search Products" class="input_search">
+    </div>
+    <button class="search_btn"><img src="images/logos_and_icons/search_icon.png" alt="search" class="search_icon"></button>
+  </div>
+  <div class="buttons">
+    <div class="orders">
+      <img src="images/logos_and_icons/orders_icon.png" alt="orders" class="orders_icon"/>
+    </div>
+    <div class="cart">
+      <img src="images/logos_and_icons/cart_icon.png" alt="cart" class="cart_icon">
+      <div class="cart_item_count_container">
+        <p class="cart_item_count">2</p>
+      </div>
+    </div>
+  </div>
+`;
+
+const header = document.querySelector('.header');
+header.innerHTML = headerData;
